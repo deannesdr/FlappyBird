38829839a2d48bbfe6277640356bc4c4ee952961
deannesdr <deannesdr13@gmail.com>
2025-02-08 15:03:40 +0800

    Added a board frame.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import javax.swing. *;  // Importing all classes from javax.swing package
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //initializing the board dimentions.
+        int boardwidth = 360;
+        int boardheight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird"); // Creating a new JFrame object
+        frame.setVisible(true); // Setting the frame to be visible
+        frame.setSize(boardwidth, boardheight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Setting the default close operation of the frame
+    
+    
     }
 }
